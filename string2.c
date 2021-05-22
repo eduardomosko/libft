8c21ee7b244c6365abc9bc2af5e365a6c6e267f5
Eduardo Mosko <eduardommosko@gmail.com>
2021-05-22 18:26:31 -0300

    ft_strrchr

--- string2.c
+++ string2.c
@@ -12,6 +12,23 @@
 
 #include "libft.h"
 
+char	*ft_strrchr(const char *s, int c)
+{
+	unsigned char	target;
+	unsigned char	*mem;
+	size_t			n;
+
+	n = ft_strlen(s);
+	if (n == 0)
+		return (NULL);
+	target = c;
+	mem = (unsigned char *)s + n - 1;
+	while (n--)
+		if (*mem-- == target)
+			return ((char *)++mem);
+	return (NULL);
+}
+
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	size_t	i;
