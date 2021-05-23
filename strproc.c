806a065e5d74517b5e58490e1ca461ee09feecc5
Eduardo Mosko <eduardommosko@gmail.com>
2021-05-23 19:37:13 -0300

    strjoin

--- strproc.c
+++ strproc.c
@@ -19,7 +19,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 	slen = ft_strlen(s);
 	if (start > slen)
-		return (NULL);
+		start = slen;
 	s += start;
 	slen -= start;
 	if (slen < len)
@@ -34,9 +34,19 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	UNUSED(s1);
-	UNUSED(s2);
-	return (NULL);
+	char	*ret;
+	size_t	len1;
+	size_t	len2;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	ret = malloc(len1 + len2 + 1);
+	if (ret == NULL)
+		return (NULL);
+	ft_memcpy(ret, s1, len1);
+	ft_memcpy(ret + len1, s2, len2);
+	ret[len1 + len2] = 0;
+	return (ret);
 }
 
 char	*ft_strtrim(char const *s1, char const *set);
