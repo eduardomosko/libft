1b0ace4cc11754320d02c54cbf21656d3f2cd1ad
Eduardo Mosko <eduardommosko@gmail.com>
2021-06-08 22:15:41 -0300

    Fixed makefile

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdint.h>
 #include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
